f051a2ae81a7d7a7722bf4826a005ee4f01b46b6
Matheus Barros <88664949+MatheusBarros23@users.noreply.github.com>
2023-11-24 13:23:18 -0300

    Add new things

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+
 
 Base = declarative_base()
 
@@ -9,6 +10,8 @@ class Team(Base):
     __tablename__ = "teams"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    players = relationship("Player", back_populates="team")
+    games = relationship("Game", back_populates="team")
 
 class Player(Base):
     __tablename__ = "players"
@@ -17,11 +20,17 @@ class Player(Base):
     team_id = Column(Integer, ForeignKey("teams.id"))
     team = relationship("Team", back_populates="players")
 
-Team.players = relationship("Player", back_populates="team")
+class Game(Base):
+    __tablename__ = "games"
 
-DATABASE_URL = "postgresql://user:password@db/db"
+    id = Column(Integer, primary_key=True, index=True)
+    team_id = Column(Integer, ForeignKey("teams.id"))
+    date = Column(Date, nullable=False)
+    location = Column(String, nullable=False)
 
-engine = create_engine(DATABASE_URL)
+    team = relationship("Team", back_populates="games")
+
+engine = create_engine("postgresql://user:password@db/db")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app = FastAPI()
@@ -36,9 +45,63 @@ def create_team(name: str):
     db.close()
     return team
 
-@app.get("/teams/{team_id}/")
-def read_team(team_id: int):
+
+
+@app.post("/players/")
+def create_player(name: str, team_id: int):
     db = SessionLocal()
-    team = db.query(Team).filter(Team.id == team_id).first()
+    player = Player(name=name, team_id=team_id)
+    db.add(player)
+    db.commit()
+    db.refresh(player)
     db.close()
-    return team
+    return player
+
+@app.post("/games/")
+def create_game(team_id: int, date: str, location: str):
+    db = SessionLocal()
+    game = Game(team_id=team_id, date=date, location=location)
+    db.add(game)
+    db.commit()
+    db.refresh(game)
+    db.close()
+    return game
+
+
+@app.get("/teams/{team_id}/players")
+def get_players_by_team(team_id: int):
+    db = SessionLocal()
+    players = db.query(Player).filter(Player.team_id == team_id).all()
+    db.close()
+    return {"players": players}
+
+@app.delete("/teams/{team_id}/players/{player_id}")
+def delete_player(team_id: int, player_id: int):
+    db = SessionLocal()
+    
+    # Verificar se o time existe
+    team = db.query(Team).filter(Team.id == team_id).first()
+    if not team:
+        db.close()
+        raise HTTPException(status_code=404, detail="Time não encontrado")
+
+    # Verificar se o jogador pertence ao time
+    player = db.query(Player).filter(Player.id == player_id, Player.team_id == team_id).first()
+    if not player:
+        db.close()
+        raise HTTPException(status_code=404, detail="Jogador não encontrado no time")
+
+    # Deletar o jogador
+    db.delete(player)
+    db.commit()
+    db.close()
+
+    return {"message": "Jogador deletado com sucesso"}
+
+@app.get("/teams/")
+def read_teams(skip: int = 0, limit: int = 10):
+    db = SessionLocal()
+    teams = db.query(Team).offset(skip).limit(limit).all()
+    db.close()
+    team_list = [{"id": team.id, "name": team.name} for team in teams]
+    return team_list
--- docker/db/create_tables.sql
+++ docker/db/create_tables.sql
@@ -1,15 +1,27 @@
-CREATE TABLE IF NOT EXISTS teams (
+CREATE TABLE IF NOT EXISTS games (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS players (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE
+    team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE,
+    date DATE NOT NULL,
+    location VARCHAR(100) NOT NULL
 );
 
 INSERT INTO teams (name) VALUES
-    ('Team A'),
-    ('Team B'),
-    ('Team C');
+    ('Team D'),
+    ('Team E'),
+    ('Team F');
+
+INSERT INTO players (name, team_id) VALUES
+    ('Player 1', 1),
+    ('Player 2', 1),
+    ('Player 3', 2),
+    ('Player 4', 2),
+    ('Player 5', 3),
+    ('Player 6', 3);
+
+INSERT INTO games (team_id, date, location) VALUES
+    (1, '2022-01-01', 'Location 1'),
+    (1, '2022-01-02', 'Location 2'),
+    (2, '2022-01-03', 'Location 3'),
+    (2, '2022-01-04', 'Location 4'),
+    (3, '2022-01-05', 'Location 5'),
+    (3, '2022-01-06', 'Location 6');
