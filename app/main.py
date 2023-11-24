d8cc22499aaa78ef01da40be68d7b849c92e9510
Matheus Barros <88664949+MatheusBarros23@users.noreply.github.com>
2023-11-24 17:01:09 -0300

    Add much more things

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ class Team(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     players = relationship("Player", back_populates="team")
-    games = relationship("Game", back_populates="team")
+    games_home = relationship("Game", foreign_keys="[Game.team_id_home]", back_populates="team_home")
+    games_away = relationship("Game", foreign_keys="[Game.team_id_away]", back_populates="team_away")
+
 
 class Player(Base):
     __tablename__ = "players"
@@ -24,11 +26,14 @@ class Game(Base):
     __tablename__ = "games"
 
     id = Column(Integer, primary_key=True, index=True)
-    team_id = Column(Integer, ForeignKey("teams.id"))
+    team_id_home = Column(Integer, ForeignKey("teams.id"))
+    team_id_away = Column(Integer, ForeignKey("teams.id"))
     date = Column(Date, nullable=False)
     location = Column(String, nullable=False)
 
-    team = relationship("Team", back_populates="games")
+    team_home = relationship("Team", foreign_keys=[team_id_home], back_populates="games_home")
+    team_away = relationship("Team", foreign_keys=[team_id_away], back_populates="games_away")
+
 
 engine = create_engine("postgresql://user:password@db/db")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -58,14 +63,23 @@ def create_player(name: str, team_id: int):
     return player
 
 @app.post("/games/")
-def create_game(team_id: int, date: str, location: str):
+def create_game(team_id_home: int, team_id_away: int, date: str, location: str):
     db = SessionLocal()
-    game = Game(team_id=team_id, date=date, location=location)
+
+    teams = db.query(Team).filter(Team.id.in_([team_id_home, team_id_away])).all()
+    if len(teams) != 2:
+        db.close()
+        raise HTTPException(status_code=404, detail="Um ou ambos os times não foram encontrados")
+
+    game = Game(date=date, location=location, team_home=teams[0], team_away=teams[1])
+
     db.add(game)
     db.commit()
     db.refresh(game)
     db.close()
+
     return game
+
 
 
 @app.get("/teams/{team_id}/players")
@@ -105,3 +119,45 @@ def read_teams(skip: int = 0, limit: int = 10):
     db.close()
     team_list = [{"id": team.id, "name": team.name} for team in teams]
     return team_list
+
+@app.get("/teams/{team_id}/details")
+def get_team_details(team_id: int):
+    db = SessionLocal()
+    team = db.query(Team).filter(Team.id == team_id).first()
+    if not team:
+        db.close()
+        raise HTTPException(status_code=404, detail="Time não encontrado")
+    
+    team_players = db.query(Player).filter(Player.team_id == team_id).all()
+    team_games_home = db.query(Game).filter(Game.team_id_home == team_id).all()
+    team_games_away = db.query(Game).filter(Game.team_id_away == team_id).all()
+
+    db.close()
+
+    games_home_details = [{"game_id": game.id, "date": game.date, "location": game.location} for game in team_games_home]
+    games_away_details = [{"game_id": game.id, "date": game.date, "location": game.location} for game in team_games_away]
+
+    team_details = {
+        "team_id": team.id,
+        "team_name": team.name,
+        "players": [{"player_id": player.id, "player_name": player.name} for player in team_players],
+        "games_home": games_home_details,
+        "games_away": games_away_details,
+    }
+
+    return team_details
+
+@app.delete("/teams/{team_id}")
+def delete_team(team_id: int):
+    db = SessionLocal()
+
+    team = db.query(Team).filter(Team.id == team_id).first()
+    if not team:
+        db.close()
+        raise HTTPException(status_code=404, detail="Time não encontrado")
+
+    db.delete(team)
+    db.commit()
+    db.close()
+
+    return {"message": "Time deletado com sucesso"}
--- docker/db/create_tables.sql
+++ docker/db/create_tables.sql
@@ -1,6 +1,19 @@
+
+CREATE TABLE IF NOT EXISTS teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS players (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS games (
     id SERIAL PRIMARY KEY,
-    team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE,
+    team_id_home INTEGER REFERENCES teams(id) ON DELETE CASCADE,
+    team_id_away INTEGER REFERENCES teams(id) ON DELETE CASCADE,
     date DATE NOT NULL,
     location VARCHAR(100) NOT NULL
 );
@@ -18,10 +31,10 @@ INSERT INTO players (name, team_id) VALUES
     ('Player 5', 3),
     ('Player 6', 3);
 
-INSERT INTO games (team_id, date, location) VALUES
-    (1, '2022-01-01', 'Location 1'),
-    (1, '2022-01-02', 'Location 2'),
-    (2, '2022-01-03', 'Location 3'),
-    (2, '2022-01-04', 'Location 4'),
-    (3, '2022-01-05', 'Location 5'),
-    (3, '2022-01-06', 'Location 6');
+INSERT INTO games (team_id_home, team_id_away, date, location) VALUES
+    (1, 2, '2022-01-01', 'Location 1'),
+    (1, 3, '2022-01-02', 'Location 2'),
+    (2, 3, '2022-01-03', 'Location 3'),
+    (2, 1, '2022-01-04', 'Location 4'),
+    (3, 1, '2022-01-05', 'Location 5'),
+    (3, 2, '2022-01-06', 'Location 6');
